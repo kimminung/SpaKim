3eecac8a438b0c02709d7206b385a3bd439afa32
민웅킴 <minogo1054@paran.com>
2024-02-28 20:36:43 +0900

    TestProject Min -> MinKim

--- TestProject/TestProject/ContentView.swift
+++ TestProject/TestProject/ContentView.swift
@@ -13,7 +13,7 @@ struct ContentView: View {
             Image(systemName: "globe")
                 .imageScale(.large)
                 .foregroundStyle(.tint)
-            Text("Min, world!")
+            Text("MinKim, world!")
         }
         .padding()
     }
