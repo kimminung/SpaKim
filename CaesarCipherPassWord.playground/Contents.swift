3a960b1872879f13f96349bab41c5f813c2d7340
kimminung <minogo1054@paran.com>
2024-03-08 20:54:39 +0900

    규식이는 못해따

--- CaesarCipherPassWord.playground/Contents.swift
+++ CaesarCipherPassWord.playground/Contents.swift
@@ -10,18 +10,41 @@ func solution(_ s:String, _ n:Int) -> String {
     guard s >= "^[A-Za-z ]*$" else {return "Err1"}
     guard s.count <= 8000 else {return "Err3"}
     
+    
     var char = [String]()
+    var result = ""
 //    var ii = 0
     for i in s {
-//         ii+=1
+        //         ii+=1
+        let ascii = Int(UnicodeScalar(String(i))!.value)
+        var count = 0
         char.append(String(i))
-        if char. >= "^[A-Za-z ]*$" {
-            
+        if char[count] >= "^[A-Z]*$" {
+            if char[count] == "^[Z]*$" {
+                result += String(UnicodeScalar((ascii + 6) + n)!).lowercased()
+            } else {
+                // print((UnicodeScalar(ascii + n)!))
+                result += String(UnicodeScalar(ascii + n)!)
+            }
+        } else if char[count] >= "^[a-z]*$"{
+            if char[count] == "^[z]*$" {
+                result += String(UnicodeScalar((ascii - 56) + n)!).uppercased()
+            } else {
+                result += String(UnicodeScalar(ascii + n)!)
+            }
+        } else if char[count] >= "^[ ]*$"{
+            result += [" "]
+        } else {
+            //            return fatalError("Error")
+            return "Err4"
         }
-        }
+        count+=1
+    }
     
     print(s)
-    return s
+    print(result)
+    return String(result)
 }
 
-solution("a  sd as", 1)
+//solution("a  sd as", 1)
+solution("a B z", 4)
