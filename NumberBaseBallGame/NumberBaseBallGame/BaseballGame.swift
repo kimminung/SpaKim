4ea8d8d38845847f6154af0af095d737fd15b1b1
kimminung <minogo1054@paran.com>
2024-03-12 16:37:21 +0900

    a

---
+++ NumberBaseBallGame/NumberBaseBallGame/BaseballGame.swift
@@ -0,0 +1,41 @@
+//
+//  BaseballGame.swift
+//  NumberBaseBallGame
+//
+//  Created by 민웅킴 on 3/12/24.
+//
+
+//import Foundation
+//
+//// BaseballGame.swift 파일 생성
+//class BaseballGame {
+//    func start() {
+//        let answer = makeAnswer() // 정답을 만드는 함수
+//        
+//    }
+//    
+//    func makeAnswer() -> Int {
+//        // 함수 내부를 구현하기
+//        return 0
+//    }
+//}
+
+
+/*
+ for _ in 0..<1 {
+    result += "\(Int.random(in: 1..<10))"
+     for _ in 0..<1 {
+         result += "\(Int.random(in: 1..<10))"
+         for _ in 0..<1 {
+             result += "\(Int.random(in: 1..<10))"
+         }
+     }
+ }
+ print(result)
+
+ for i in n {
+     if i == Int(result.prefix(0)) {
+         print("\(i+1)strike")
+     }
+ }
+ */
--- NumberBaseBallGame/NumberBaseBallGame/main.swift
+++ NumberBaseBallGame/NumberBaseBallGame/main.swift
@@ -1,6 +1,15 @@
 import Foundation
 
 
+//let game = BaseballGame()
+//game.start() // BaseballGame 인스턴스를 만들고 start 함수를 구현하기
+//
+//// readLine() 함수를 이용하여 유저의 입력값 처리하기
+//// readLine() 함수에 대해 학습해보고 활용하기
+//let input = readLine()
+
+
+
 //처음 만들려고 했던것
 let n = readLine()!.components(separatedBy: [" "]).map { Int($0)! }
 //let (a, b, c) = (n[0], n[1], n[2])
@@ -23,5 +32,7 @@ for i in n {
         print("\(i+1)strike")
     }
 }
+
+
 
 
